ad5ba8d79a5f5532c69020a7dea3db0b465dc458
John Roesler <jroesler@gmail.com>
2014-06-30 07:53:40 -0500

    add money market account type

--- src/main/scala/org/vvcephei/scalaofx/lib/model/AccountType.java
+++ src/main/scala/org/vvcephei/scalaofx/lib/model/AccountType.java
@@ -1,7 +1,7 @@
 package org.vvcephei.scalaofx.lib.model;
 
 public enum AccountType {
-    CHECKING,SAVINGS;
+    CHECKING,SAVINGS,MONEYMRKT;
 
     public static AccountType from(String name) {
         return valueOf(name.toUpperCase());
